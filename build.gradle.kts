eec48eff94e0a3ca15d2e3ca8d083c31b618b56f
Llonvne <86454692+Llonvne@users.noreply.github.com>
2023-12-11 22:22:35 +0800

    publish prepare

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,73 @@
 plugins {
     kotlin("jvm") version "1.9.20" apply false
     id("com.bennyhuo.kotlin.plugin.embeddable.test") version "1.8.1" apply false
+    id("maven-publish")
+    signing
+
+}
+
+repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+}
+
+group = "cn.llonvne"
+version = "0.0.1"
+
+publishing {
+    publications {
+
+        create<MavenPublication>("maven") {
+            artifact("javadocTask")
+
+            groupId = "cn.llonvne"
+            artifactId = "PropertiesCopy"
+            version = "0.0.1"
+
+            pom {
+                name = "PropertiesCopy"
+                description = "Copy Properties From Class to Class"
+
+                licenses {
+                    name = "MIT"
+                    url = "https://github.com/Llonvne/PropertiesCopy/blob/main/LICENSE"
+                }
+
+                developers {
+                    developer {
+                        id = "llonvne"
+                        name = "llonvne"
+                        email = "Llonvne@gmail.com"
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/llonvne/PropertiesCopy.git")
+                    developerConnection.set("scm:git:ssh://github.com:llonvne/PropertiesCopy.git")
+                    url.set("https://github.com/llonvne/PropertiesCopy")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "PropertiesCopy"
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            setUrl { snapshotRepoUrl }
+        }
+    }
+}
+
+signing {
+    signing {
+        useInMemoryPgpKeys(
+            System.getProperty("GPG_PRIVATE_KEY"),
+            System.getProperty("GPG_PRIVATE_PASSWORD")
+        )
+        sign(publishing.publications)
+    }
 }
 
 allprojects {
@@ -8,3 +75,4 @@ allprojects {
         extensions.getByType<JavaPluginExtension>().sourceCompatibility = JavaVersion.VERSION_1_8
     }
 }
+
